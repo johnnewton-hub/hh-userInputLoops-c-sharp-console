7952d622c6deab4b09147f6b4dfef5d4868ca86e
AaronChampagne <aaron.m.champagne@gmail.com>
2020-06-28 11:40:17 -0600

    Two input loops that will force the user to enter an int, and enter an int between 1 and 9

---
+++ UserInputLoops/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserInputLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int validatedInt = Prompt.promptForInt();
+            validatedInt = Prompt.promptForIntBetween1And9();
+
+            Console.WriteLine("Thank you for entering the number " + validatedInt.ToString());
+        }
+
+
+    }
+}
---
+++ UserInputLoops/Prompt.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserInputLoops
+{
+    public class Prompt
+    {
+        public static int promptForInt()
+        {
+            int parsedInput;
+            bool isInt;
+            do
+            {
+                Console.WriteLine("Enter a number:");
+                string input = Console.ReadLine();
+
+                isInt = int.TryParse(input, out parsedInput);
+
+                //Add error messages to help your user figure it out
+                if (!isInt)
+                {
+                    Console.WriteLine("Try again, but at least enter a number this time.");
+                }
+
+            } while (!isInt);
+            return parsedInput;
+        }
+
+        public static int promptForIntBetween1And9()
+        {
+            int parsedInput;
+            do
+            {
+                Console.WriteLine("Enter a number between 1 and 9");//0 and 10 are bad
+                string input = Console.ReadLine();
+                //explicit version with exception handling (exception means error)
+                //try
+                //{
+                //    parsedInput = int.Parse(input);
+                //} catch (Exception e)
+                //{
+                //    Console.WriteLine(e.Message);
+                //}
+
+                //elegant version, but without error handling.
+                bool isInt = int.TryParse(input, out parsedInput);
+
+                //Add error messages to help your user figure it out
+                if (!isInt)
+                {
+                    Console.WriteLine("Try again, but at least enter a number this time.");
+                }
+                if (parsedInput < 1 && parsedInput != 0)
+                {
+                    Console.WriteLine("Almost there, pick a higher number.");
+                }
+                if (parsedInput > 9)
+                {
+                    Console.WriteLine("Almost there, pick a lower number.");
+                }
+
+            } while (parsedInput > 10 || parsedInput < 1);
+            return parsedInput;
+        }
+    }
+}
